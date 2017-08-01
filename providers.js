6b5ead9455004a497d62eed85b9c30615c3e5ede
el-davo <david.ahern26@gmail.com>
2017-08-01 08:46:16 +0100

    Adding github login

--- providers.js
+++ providers.js
@@ -2,6 +2,8 @@
 
 const FACEBOOK_CLIENTID = process.env.facebookClientId;
 const FACEBOOK_CLIENT_SECRET = process.env.facebookClientSecret;
+const GITHUB_CLIENT_ID = process.env.githubCLientId;
+const GITHUB_CLIENT_SECRET = process.env.githubClientSecret;
 
 module.exports = {
   'facebook-login': {
@@ -14,7 +16,16 @@ module.exports = {
     authPath: '/auth/facebook',
     successRedirect: '/client/auth.html',
     scope: ['email', 'public_profile'],
-    profileFields: ['gender', 'locale', 'name', 'timezone', 'email', 'picture'],
+    profileFields: ['gender', 'locale', 'name', 'timezone', 'email', 'picture']
   },
+  github: {
+    provider: 'github',
+    module: 'passport-github',
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: '/auth/github/callback',
+    authPath: '/auth/github',
+    successRedirect: '/client/auth.html'
+  }
 };
 
