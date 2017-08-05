61ebed477fb1541bacdac73ce579abd47e839d20
el-davo <david.ahern26@gmail.com>
2017-08-05 15:48:52 +0100

    Adding validations

---
+++ common/models/list.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = List => {
+  // Validations
+  List.validatesLengthOf('name', {
+    min: 1,
+    max: 256,
+    message: {
+      min: 'Name is to short',
+      max: 'Name is to long'
+    }
+  });
+
+  List.validatesLengthOf('description', {
+    min: 1,
+    max: 500,
+    message: {
+      min: 'Description is to short',
+      max: 'Description is to long'
+    }
+  });
+};
