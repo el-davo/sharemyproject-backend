64bae79c97698fa0629a2c05b76ce258ab5a38e8
el-davo <david.ahern26@gmail.com>
2017-08-13 14:41:27 +0100

    Updated

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,11 +1,15 @@
 'use strict';
 
 let sqlUrl = process.env.sql_url;
+let sqlUsername = process.env.sql_username;
+let sqlPassword = process.env.sql_password;
 
 module.exports = {
   db: {
     connector: 'mysql',
     connectionLimit: 4,
-    url: sqlUrl
+    url: sqlUrl,
+    username: sqlUsername,
+    password: sqlPassword
   }
 };
